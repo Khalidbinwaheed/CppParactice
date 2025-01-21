b3f95650aa2135991b0fedc622817f2df7773030
Khalid Bin Waheed <captainkhalid20@gmail.com>
2025-01-21 18:16:06 +0500

    full

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 using namespace std;
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-};
 
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
 
-struct Node* newNode(int data){
-    struct Node* node = new struct Node;
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-} 
-int main(){
-    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
 
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1; // Target not found
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 5;
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found" << endl;
 
     return 0;
 }
