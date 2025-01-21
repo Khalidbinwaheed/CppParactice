6a68593e11f46118cdbbd1681971eafed8459989
Khalid Bin Waheed <captainkhalid20@gmail.com>
2025-01-21 16:08:17 +0500

    new

--- Queue.cpp
+++ Queue.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include <iostream>
 #include <queue>
 using namespace std;
+
 int main()
 {
-    Queue <int> myQueue;
+    queue<int> myQueue;  // Correct type
     myQueue.push(1);
     myQueue.push(2);
     myQueue.push(3);
     myQueue.push(4);
     myQueue.push(5);
 
+    cout << "Size of the queue is: " << myQueue.size() << endl;
+    cout << "Front element of the queue is: " << myQueue.front() << endl;
+    cout << "Back element of the queue is: " << myQueue.back() << endl;
 
-    cout <<"Size of the queue is: "<< myQueue.size() << endl;
-    cout << "Front element of the queue is: "<< myQueue.front() << endl;
-    cout << "Back element of the queue is: "<< myQueue.back() << endl;
-    cout << "Element popped from the queue is: "<< myQueue.pop() << endl;
+    cout << "Element popped from the queue is: " << myQueue.front() << endl; // Access the front element
+    myQueue.pop(); // Remove the front element
+
+    return 0;
 }
