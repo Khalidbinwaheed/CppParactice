c56042fcc1e25da9dd406bc0a6b7c50ff95cffc2
Khalid Bin Waheed <captainkhalid20@gmail.com>
2025-01-22 07:41:50 +0500

    binary tree

---
+++ binarytreesearch.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    if (!newNode) {
+        cout << "Memory error\n";
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+void inOrder(Node* temp) {
+    if (temp == NULL)
+        return;
+
+    inOrder(temp->left);
+    cout << temp->data << ' ';
+    inOrder(temp->right);
+}
+
+int main() {
+    Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    cout << "In-order traversal: ";
+    inOrder(root);
+    cout << endl;
+
+    return 0;
+}
